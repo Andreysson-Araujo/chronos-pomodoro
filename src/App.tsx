fb240cb3006cb18a041059d908ee5ed6aaa7519b
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-10-13 13:22:40 -0500

    escopo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Heading } from "./components/Heading";
 
 export default function App() {
   return (
     <>
       <div>
-        <h1>Ola</h1>
+        <Heading />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
           placeat explicabo ut autem sequi dolorem enim est rem nam quod
---
+++ src/components/Heading.tsx
@@ -0,0 +1,5 @@
+import "./Heading.mudule.css";
+
+export function Heading() {
+  return <h1 className="heading">Ola mundo</h1>;
+}
