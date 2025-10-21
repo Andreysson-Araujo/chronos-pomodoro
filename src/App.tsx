08db3880b4ddd3318cb4d203d5001a5e478313c9
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-10-21 10:33:47 -0500

    Moving the Container to its own React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
 
 export default function App() {
   return (
     <>
-      <Heading>
-        Ola Mundo2
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-        placeat explicabo ut autem sequi dolorem enim est rem nam quod officia,
-        voluptatibus consectetur at. Nisi delectus id doloremque animi dolorem.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
