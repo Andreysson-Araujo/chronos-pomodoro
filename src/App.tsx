77be1a87566f9bee848709f3e7ba72f4afcc79d7
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-10-21 09:49:46 -0500

    add Typagem ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
 export default function App() {
   return (
     <>
-      <div>
-        <Heading />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-          placeat explicabo ut autem sequi dolorem enim est rem nam quod
-          officia, voluptatibus consectetur at. Nisi delectus id doloremque
-          animi dolorem.
-        </p>
-      </div>
+      <Heading>
+        Ola Mundo2
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        placeat explicabo ut autem sequi dolorem enim est rem nam quod officia,
+        voluptatibus consectetur at. Nisi delectus id doloremque animi dolorem.
+      </p>
     </>
   );
 }
